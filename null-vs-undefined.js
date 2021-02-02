39829dc9b259cf2afb764548ec852afd0ac76149
noorhossain <noorsan809@gmail.com>
2021-02-02 17:03:27 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2)
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2)
+//     return
+// }
+
+
+// const premik = {name: "sabrina", phone:0978735};
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages = [12, 14, 15];
+console.log(ages[5]);
