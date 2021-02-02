192d8036edf13e165bc649337c22c5c2dd4289bc
noorhossain <noorsan809@gmail.com>
2021-02-02 16:45:35 +0600

    uploaded

---
+++ true-false.js
@@ -0,0 +1,22 @@
+// Falsy
+// ""
+// 0
+// undefined
+// null
+// NaN
+// false
+
+// Truthy
+// "0"
+// " "
+// []
+// {}
+
+
+let name = 29;
+if(name || name == 0){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
